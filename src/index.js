afa798234c4b3d4a5e8cb0062a6ffbb88b72ece5
nRike <nrikediaz@gmail.com>
2018-10-20 23:21:44 -0500

    Yay, we finished the project

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ class Game extends React.Component {
     }
 
     handleClick(i){
-        const history = this.state.history.slice(0, this.state.stepNumber);
+        const history = this.state.history.slice(0, this.state.stepNumber+1);
         const current = history[history.length -1];
         const squares = current.squares.slice();
 
